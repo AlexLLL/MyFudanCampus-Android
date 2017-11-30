6772c73386c5aedaa922439154f509869df196f5
alex <alex@pool-100-1-26-241.nwrknj.fios.verizon.net>
2017-12-01 00:30:42 +0800

    finish tab

--- MyFudanCampus-Android/app/src/main/java/com/example/myfudancampus/CampusActivity.java
+++ MyFudanCampus-Android/app/src/main/java/com/example/myfudancampus/CampusActivity.java
@@ -6,39 +6,70 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
 
 public class CampusActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private TextView txt_main;
+    private BottomNavigationBar bottom_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus);
+        initView();
+        initEvent();
+    }
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    private void initEvent() {
+        bottom_bar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                switch (position) {
+                    case 0:
+                        txt_main.setText(getString(R.string.title_news));
+                        break;
+                    case 1:
+                        txt_main.setText(getString(R.string.title_gpa));
+                        break;
+                    case 2:
+                        txt_main.setText(getString(R.string.title_bbs));
+                        break;
+                    case 3:
+                        txt_main.setText(getString(R.string.title_about));
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        txt_main = (TextView) findViewById(R.id.txt_main);
+        bottom_bar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+
+        bottom_bar
+                .setActiveColor(R.color.skyblue)
+                .setMode(BottomNavigationBar.MODE_FIXED)
+                .addItem(new BottomNavigationItem(R.drawable.ic_home_black_24dp, "Home"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_home_black_24dp, "Books"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_notifications_black_24dp, "Music"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_notifications_black_24dp, "Movies & TV"))
+                .initialise();
+        txt_main.setText(getString(R.string.title_news));
+
     }
 
 }
+
